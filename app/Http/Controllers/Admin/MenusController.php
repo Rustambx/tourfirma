1a9009a270133f8d55df81f9276c4ccefe18c591
Rustam Home <rustambx@gmail.com>
2019-11-29 15:19:09 +0500

    Подключил Gate на MenusController

--- app/Http/Controllers/Admin/MenusController.php
+++ app/Http/Controllers/Admin/MenusController.php
@@ -7,6 +7,7 @@ use App\Models\Menu;
 use App\Repositories\MenusRepository;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Gate;
 
 class MenusController extends AdminController
 {
